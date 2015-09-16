62d9d93b5dd40029aa719d020728003c844742b7
Tobias Pfeiffer <tobi@bitcrowd.net>
2015-09-16 21:08:04 +0200

    more cleanup

--- spec/help/class_aliases.rb
+++
@@ -1,7 +0,0 @@
-# I am a bit lazy so I alias all the classes important for testing here
-
-Board                = Rubykon::Board
-IllegalMoveException = Rubykon::IllegalMoveException
-StoneFactory         = Rubykon::SpecHelpers::StoneFactory
-MoveValidator        = Rubykon::MoveValidator
-Stone                = Rubykon::Stone
--- spec/help/stone_factory.rb
+++ spec/help/stone_factory.rb
@@ -1,18 +1,17 @@
 # A simple factory generating valid moves for board sizes starting 9
 module Rubykon
   module StoneFactory
+    extend self
 
     DEFAULT_X     = 5
     DEFAULT_Y     = 9
     DEFAULT_COLOR = :black
 
-    class << self
-      def build(options = {})
-        x     = options[:x]     || DEFAULT_X
-        y     = options[:y]     || DEFAULT_Y
-        color = options[:color] || DEFAULT_COLOR
-        Rubykon::Stone.new x, y, color
-      end
+    def build(options = {})
+      x     = options[:x]     || DEFAULT_X
+      y     = options[:y]     || DEFAULT_Y
+      color = options[:color] || DEFAULT_COLOR
+      Rubykon::Stone.new x, y, color
     end
   end
 end
