2ea492cdcadbb87389802132f29395b12d127286
Tobias Pfeiffer <tobi@bitcrowd.net>
2015-09-18 20:34:53 +0200

    Code Climate test coverage

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source :rubygems
 
 gem 'rspec', '~> 3.3'
 gem 'guard-rspec'
+
+gem "codeclimate-test-reporter", group: :test, require: nil
--- spec/rubykon/spec_helper.rb
+++ spec/rubykon/spec_helper.rb
@@ -1,2 +1,5 @@
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 require_relative '../../lib/rubykon'
 require_relative 'help/stone_factory'
