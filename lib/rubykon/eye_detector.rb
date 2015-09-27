24f408f9233969fa23d911ff4306c94c0201b620
Tobias Pfeiffer <pragtob@gmail.com>
2015-09-27 14:21:10 +0200

    board specs pass again...

--- lib/rubykon/eye_detector.rb
+++ lib/rubykon/eye_detector.rb
@@ -1,14 +1,14 @@
 module Rubykon
   class EyeDetector
-    def is_eye?(x, y, board)
-      candidate_eye_color = candidate_eye_color(x, y, board)
+    def is_eye?(identifier, board)
+      candidate_eye_color = candidate_eye_color(identifier, board)
       return false unless candidate_eye_color
-      is_real_eye?(x, y, board, candidate_eye_color)
+      is_real_eye?(identifier, board, candidate_eye_color)
     end
 
     private
-    def candidate_eye_color(x, y, board)
-      neighbor_colors = board.neighbour_colors_of(x, y)
+    def candidate_eye_color(identifier, board)
+      neighbor_colors = board.neighbour_colors_of(identifier)
       candidate_eye_color = neighbor_colors.first
       return false if candidate_eye_color == Board::EMPTY
       if neighbor_colors.all? {|color| color == candidate_eye_color}
@@ -18,10 +18,10 @@ module Rubykon
       end
     end
 
-    def is_real_eye?(x, y, board, candidate_eye_color)
+    def is_real_eye?(identifier, board, candidate_eye_color)
       enemy_color = Stone.other_color(candidate_eye_color)
-      enemy_count = board.diagonal_colors_of(x, y).count(enemy_color)
-      (enemy_count < 1) || (!board.on_edge?(x, y) && enemy_count < 2)
+      enemy_count = board.diagonal_colors_of(identifier).count(enemy_color)
+      (enemy_count < 1) || (!board.on_edge?(identifier) && enemy_count < 2)
     end
   end
 end
--- spec/rubykon/board_spec.rb
+++ spec/rubykon/board_spec.rb
@@ -6,35 +6,52 @@ module Rubykon
 
     context 'setting and retrieving LOOKUP' do
       it 'has the empty symbol for every LOOKUP' do
-        all_empty = board.all? do |field|
-          field.color == Board::EMPTY
+        all_empty = board.all? do |identifier, color|
+          color == Board::EMPTY
         end
         expect(all_empty).to be true
       end
 
       it 'can retrive the empty values via #[]' do
-        expect(board[1, 1].color).to eq Board::EMPTY
+        expect(board[1]).to eq Board::EMPTY
       end
 
       it "gives the initially set stones the right coordinates" do
-        expect(board[1, 1]).to eq Stone.new 1, 1, Board::EMPTY
-        expect(board[1, 7]).to eq Stone.new 1, 7, Board::EMPTY
-        expect(board[7, 1]).to eq Stone.new 7, 1, Board::EMPTY
-        expect(board[19, 19]).to eq Stone.new 19, 19, Board::EMPTY
-        expect(board[3, 5]).to eq Stone.new 3, 5, Board::EMPTY
+        expect(board[board.identifier_for(1, 1)]).to eq Board::EMPTY
+        expect(board[board.identifier_for(1, 7)]).to eq Board::EMPTY
+        expect(board[board.identifier_for(7, 1)]).to eq Board::EMPTY
+        expect(board[board.identifier_for(19, 19)]).to eq Board::EMPTY
+        expect(board[board.identifier_for(3, 5)]).to eq Board::EMPTY
       end
 
       it 'can set values with []= and geht them with []' do
-        board[1, 1] = :test
-        expect(board[1, 1]).to be :test
+        board[34] = :test
+        expect(board[34]).to be :test
+      end
+
+      it 'sets and gets with and without identifiers' do
+        identifier = board.identifier_for(5, 7)
+        board[identifier] = :special
+        expect(board[identifier]).to eq :special
       end
     end
 
-    describe "#set" do
-      it "sets to the right coordinates" do
-        stone = Stone.new(3, 7, :black)
-        board.set(stone)
-        expect(board[3, 7]).to be stone
+    describe "identifier conversion" do
+
+      it "works fine on the first line" do
+        expect(board.identifier_for(1, 1)).to eq 0
+      end
+
+      it "works fine at the last spot" do
+        expect(board.identifier_for(19, 19)).to eq 360
+      end
+
+      it "is a reversible operation" do
+        identifier = board.identifier_for(7, 9)
+        expect(identifier).to eq 158
+        x, y = board.x_y_from(identifier)
+        expect(x).to eq 7
+        expect(y).to eq 9
       end
     end
 
@@ -45,12 +62,13 @@ module Rubykon
 O-X
 ---
         String
-        expect(board.neighbours_of(2, 2)).to contain_exactly(
-                                            Stone.new(2, 1, :black),
-                                            Stone.new(3, 2, :black),
-                                            Stone.new(1, 2, :white),
-                                            Stone.new(2, 3, Board::EMPTY))
-        expect(board.neighbour_colors_of(2, 2)).to contain_exactly(
+        identifier = board.identifier_for(2, 2)
+        expect(board.neighbours_of(identifier)).to contain_exactly(
+                                          [board.identifier_for(2, 1), :black],
+                                          [board.identifier_for(3, 2), :black],
+                                          [board.identifier_for(1, 2), :white],
+                                          [board.identifier_for(2, 3), Board::EMPTY])
+        expect(board.neighbour_colors_of(identifier)).to contain_exactly(
                                                     :black, :black, :white,
                                                     Board::EMPTY)
       end
@@ -62,14 +80,32 @@ O-X
 -O-
 ---
         String
-        expect(board.neighbours_of(2, 1)).to contain_exactly(
-                                            Stone.new(3, 1, :black),
-                                            Stone.new(2, 2, :white),
-                                            Stone.new(1, 1, Board::EMPTY))
-        expect(board.neighbour_colors_of(2, 1)).to contain_exactly(
+        identifier = board.identifier_for(2, 1)
+        expect(board.neighbours_of(identifier)).to contain_exactly(
+                                            [board.identifier_for(3, 1), :black],
+                                            [board.identifier_for(2, 2), :white],
+                                            [board.identifier_for(1, 1), Board::EMPTY])
+        expect(board.neighbour_colors_of(identifier)).to contain_exactly(
                                                      :black, :white,
                                                      Board::EMPTY)
       end
+
+      it "on the other edge" do
+        board = Rubykon::Board.from <<-String
+X--
+-O-
+---
+        String
+        identifier = board.identifier_for(1, 2)
+        expect(board.neighbours_of(identifier)).to contain_exactly(
+                                            [board.identifier_for(1, 1), :black],
+                                            [board.identifier_for(2, 2), :white],
+                                            [board.identifier_for(1, 3), Board::EMPTY])
+        expect(board.neighbour_colors_of(identifier)).to contain_exactly(
+                                                     :black, :white,
+                                                     Board::EMPTY)
+      end
+
 
       it "returns fewer stones when in the corner" do
         board = Rubykon::Board.from <<-String
@@ -77,10 +113,11 @@ O-X
 ---
 ---
         String
-        expect(board.neighbours_of(1, 1)).to contain_exactly(
-                                           Stone.new(2, 1, :black),
-                                           Stone.new(1, 2, Board::EMPTY))
-        expect(board.neighbour_colors_of(1, 1)).to contain_exactly(
+        identifier = board.identifier_for(1, 1)
+        expect(board.neighbours_of(identifier)).to contain_exactly(
+                                           [board.identifier_for(2, 1), :black],
+                                           [board.identifier_for(1, 2), Board::EMPTY])
+        expect(board.neighbour_colors_of(identifier)).to contain_exactly(
                                                      :black, Board::EMPTY)
       end
     end
@@ -92,7 +129,7 @@ O-X
 ---
 X--
         BOARD
-        expect(board.diagonal_colors_of(2, 2)).to contain_exactly :white,
+        expect(board.diagonal_colors_of(board.identifier_for(2, 2))).to contain_exactly :white,
                                                                   :black,
                                                                   :black,
                                                                   Board::EMPTY
@@ -104,7 +141,7 @@ X--
 O-X
 -O-
         BOARD
-        expect(board.diagonal_colors_of(2, 2)).to contain_exactly Board::EMPTY,
+        expect(board.diagonal_colors_of(board.identifier_for(2, 2))).to contain_exactly Board::EMPTY,
                                                                   Board::EMPTY,
                                                                   Board::EMPTY,
                                                                   Board::EMPTY
@@ -116,7 +153,17 @@ O-X
 O-X
 ---
         BOARD
-        expect(board.diagonal_colors_of(2, 1)).to contain_exactly :white,
+        expect(board.diagonal_colors_of(board.identifier_for(2, 1))).to contain_exactly :white,
+                                                                  :black
+      end
+
+      it "works on the edge 2" do
+        board = Board.from <<-BOARD
+-X-
+---
+-O-
+        BOARD
+        expect(board.diagonal_colors_of(board.identifier_for(1, 2))).to contain_exactly :white,
                                                                   :black
       end
 
@@ -126,7 +173,7 @@ O-X
 -X-
 ---
         BOARD
-        expect(board.diagonal_colors_of(1, 1)).to contain_exactly :black
+        expect(board.diagonal_colors_of(board.identifier_for(1, 1))).to contain_exactly :black
       end
     end
 
@@ -134,20 +181,20 @@ O-X
       let(:board) {Board.new 5}
 
       it "is false for coordinates close to the edge" do
-        expect(board.on_edge?(2, 2)).to be_falsey
-        expect(board.on_edge?(4, 4)).to be_falsey
+        expect(board.on_edge?(board.identifier_for(2, 2))).to be_falsey
+        expect(board.on_edge?(board.identifier_for(4, 4))).to be_falsey
       end
 
       it "is true if one coordinate is 1" do
-        expect(board.on_edge?(1, 3)).to be_truthy
-        expect(board.on_edge?(2, 1)).to be_truthy
-        expect(board.on_edge?(1, 1)).to be_truthy
+        expect(board.on_edge?(board.identifier_for(1, 3))).to be_truthy
+        expect(board.on_edge?(board.identifier_for(2, 1))).to be_truthy
+        expect(board.on_edge?(board.identifier_for(1, 1))).to be_truthy
       end
 
       it "is true if one coordinate is boardsize" do
-        expect(board.on_edge?(2, 5)).to be_truthy
-        expect(board.on_edge?(5, 1)).to be_truthy
-        expect(board.on_edge?(5, 5)).to be_truthy
+        expect(board.on_edge?(board.identifier_for(2, 5))).to be_truthy
+        expect(board.on_edge?(board.identifier_for(5, 1))).to be_truthy
+        expect(board.on_edge?(board.identifier_for(5, 5))).to be_truthy
       end
     end
 
@@ -168,23 +215,23 @@ O-X
       it "is false if one of the boards has a move played" do
         board = Rubykon::Board.new 5
         other_board = Rubykon::Board.new 5
-        board[1, 1] = Rubykon::StoneFactory.build
+        board[1] = :muh
         expect(board == other_board).to be false
       end
 
       it "is true if both boards has a move played" do
         board = Rubykon::Board.new 5
         other_board = Rubykon::Board.new 5
-        board[1, 1] = Rubykon::StoneFactory.build
-        other_board[1, 1] = Rubykon::StoneFactory.build
+        board[1] = :black
+        other_board[1] = :black
         expect(board == other_board).to be true
       end
 
       it "is false if both boards have a move played but different colors" do
         board = Rubykon::Board.new 5
         other_board = Rubykon::Board.new 5
-        board[1, 1] = Rubykon::StoneFactory.build color: :white
-        other_board[1, 1] = Rubykon::StoneFactory.build color: :black
+        board[1] = :white
+        other_board[1] = :black
         expect(board == other_board).to be false
       end
     end
@@ -209,7 +256,7 @@ O-X
       end
 
       it "correctly outputs a board with a black move" do
-        board[4, 4] = Rubykon::Stone.new 4, 4, :black
+        board[board.identifier_for(4, 4)] = :black
         expected = <<-BOARD
 -------
 -------
@@ -225,7 +272,7 @@ O-X
       end
 
       it "correctly outputs a board with a white move" do
-        board[4, 4] = Rubykon::Stone.new 4, 4, :white
+        board[board.identifier_for(4, 4)] = :white
         expected = <<-BOARD
 -------
 -------
@@ -241,12 +288,12 @@ O-X
       end
 
       it "correctly outputs multiple moves played" do
-        board[1, 1] = Rubykon::Stone.new 1, 1, :white
-        board[7, 7] = Rubykon::Stone.new 7, 7, :black
-        board[1, 7] = Rubykon::Stone.new 1, 7, :white
-        board[7, 1] = Rubykon::Stone.new 7, 1, :black
-        board[5, 5] = Rubykon::Stone.new 5, 5, :white
-        board[3, 3] = Rubykon::Stone.new 3, 3, :black
+        board[board.identifier_for(1, 1)] = :white
+        board[board.identifier_for(7, 7)] = :black
+        board[board.identifier_for(1, 7)] = :white
+        board[board.identifier_for(7, 1)] = :black
+        board[board.identifier_for(5, 5)] = :white
+        board[board.identifier_for(3, 3)] = :black
 
         expected = <<-BOARD
 O-----X
