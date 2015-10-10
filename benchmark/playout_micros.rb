949f60dc90dc92632bb4fe96e5a013c00b837279
Tobias Pfeiffer <pragtob@gmail.com>
2015-10-10 21:41:35 +0200

    finalized rewrite of micros

--- benchmark/playout_micros.rb
+++ benchmark/playout_micros.rb
@@ -14,7 +14,14 @@ end
 
 class Rubykon::MoveValidator
   public :no_suicide_move?
-  public :no_ko_move?
+end
+
+class Rubykon::GroupTracker
+  public :color_to_neighbour
+  public :create_own_group
+  public :take_liberties_of_enemies
+  public :join_group_of_friendly_stones
+  public :add_liberties
 end
 
 
@@ -95,104 +102,87 @@ Benchmark.ips do |benchmark|
     benchmark.report "#{description}: set_valid_move" do
       game.dup.set_valid_move rand(361), color
     end
+
+    benchmark.report "#{description}: assign" do
+      group_tracker = game.group_tracker.dup
+      group_tracker.assign(rand(361), color, board)
+    end
+
+    group_tracker = game.group_tracker.dup
+
+    benchmark.report "#{description}: color_to_neighbour" do
+      group_tracker.color_to_neighbour(board, rand(361))
+    end
   end
 
 
-  #
-  # benchmark.report 'set' do
-  #   board.set move
-  # end
-  #
-  # stone = move
-  #
-  # benchmark.report 'assign' do
-  #   Rubykon::Group.assign(stone, board.dup)
-  # end
-  #
-  # benchmark.report 'color_to_neighbour' do
-  #   Rubykon::Group.color_to_neighbour(board, stone)
-  # end
-  #
-  # # benchmark.report 'create_own_group' do
-  # #   Rubykon::Group.create_own_group(stone)
-  # # end
-  #
-  #
-  # # All of these methods mutate state of the groups and stones, that's why
-  # # I made methods and moved them into the loop, as whatever they do when
-  # # called with already mutated state again and again, isn't normal - might
-  # # be enough for benchmarks and less interfering than creating all that
-  # # test data - you can well move them outside again :)
-  #
-  # # note that I set the coordinates and color here to make sense, the code
-  # # doesn't care about that though (that this data is right is made sure before)
-  # # so these could be any stones
-  #
-  # # these are nonsense value though
-  # empty_stone = Rubykon::Stone.new(8, 8, Rubykon::Board::EMPTY_COLOR)
-  # enemy_stone = Rubykon::Stone.new(4, 3, :white)
-  #
-  #
-  # def create_stone_1(empty_stone, enemy_stone)
-  #   stone = Rubykon::Stone.new(2, 1, :black)
-  #   liberties_1 = {
-  #     '2-2' => empty_stone,
-  #     '2-4' => enemy_stone,
-  #     '3-5' => empty_stone,
-  #     '4-6' => empty_stone,
-  #     '7-6' => empty_stone
-  #   }
-  #   Rubykon::Group.new(stone, 4, liberties_1)
-  #   stone
-  # end
-  #
-  # def create_stone_2(empty_stone, enemy_stone)
-  #   stone = Rubykon::Stone.new(2, 3, :black)
-  #   liberties_2 = {
-  #     '2-2' => empty_stone,
-  #     '2-4' => enemy_stone,
-  #     '6-5' => empty_stone,
-  #     '3-6' => empty_stone,
-  #     '7-6' => empty_stone
-  #   }
-  #   Rubykon::Group.new(stone, 4, liberties_2)
-  #   stone
-  # end
-  #
-  #
-  # # "small groups" as they have few liberties and stones assigned to them,
-  # # groups can easily have 20+ stones and even more liberties, but that'd
-  # # be even more setup :)
-  # benchmark.report 'connecting two small groups' do
-  #   stones = [create_stone_1(empty_stone, enemy_stone),
-  #             create_stone_2(empty_stone, enemy_stone)]
-  #   my_stone = Rubykon::Stone.new(2, 2, :black)
-  #   Rubykon::Group.join_group_of_friendly_stones(stones, my_stone)
-  # end
-  #
-  # benchmark.report 'add_liberties' do
-  #   liberties = [[2, 4], [2, 2], [2, 1], [1, 2]].map do |x, y|
-  #     Rubykon::Stone.new(x, y, Rubykon::Board::EMPTY_COLOR)
-  #   end
-  #   Rubykon::Group.add_liberties(liberties, create_stone_1(empty_stone, enemy_stone))
-  # end
-  #
-  #
-  # # Does not trigger enemy_group.caught? and removing the group.
-  # # That doesn't happen THAT often and it'd require to setup an according
-  # # board (with each test run)
-  # benchmark.report 'remove liberties of enemies' do
-  #   enemy_stones = [Rubykon::Stone.new(2, 3, :white),
-  #                   Rubykon::Stone.new(3, 5, :white)]
-  #   liberties = {
-  #     '2-1' => empty_stone,
-  #     '2-4' => enemy_stone,
-  #     '3-2' => empty_stone
-  #   }
-  #   Rubykon::Group.new(enemy_stones[0], 2, liberties)
-  #   Rubykon::Group.new(enemy_stones[1], 2, liberties.dup)
-  #   Rubykon::Group.take_liberties_of_enemies(enemy_stones,
-  #                                create_stone_1(empty_stone, enemy_stone),
-  #                                nil)
-  # end
+  # more rigorous setup, values gotta be right so we can't just take
+  # our randomly played out boards. Also this doesn't make much sense
+  # on the empty or finished board.
+
+  game = Rubykon::Game.new(19)
+  group_tracker = game.group_tracker
+
+  stone1 = 55
+  liberties_1 = {
+    54 => Rubykon::Board::EMPTY,
+    56 => 56,
+    36 => Rubykon::Board::EMPTY,
+    74 => Rubykon::Board::EMPTY,
+    57 => Rubykon::Board::EMPTY
+  }
+  group_1 = Rubykon::Group.new(stone1, [stone1], liberties_1, 4)
+  group_tracker.stone_to_group[55] = 1
+  group_tracker.groups[1] = group_1
+  group_tracker.create_own_group(56)
+
+  stone2 = 33
+  liberties_2 = {
+    32 => Rubykon::Board::EMPTY,
+    34 => 34,
+    24 => Rubykon::Board::EMPTY,
+    54 => Rubykon::Board::EMPTY,
+    55 => Rubykon::Board::EMPTY
+  }
+  group_2 = Rubykon::Group.new(stone2, [stone2], liberties_2, 4)
+  group_tracker.stone_to_group[33] = 2
+  group_tracker.groups[2] = group_2
+  group_tracker.create_own_group(34)
+
+  # "small groups" as they have few liberties and stones assigned to them,
+  # groups can easily have 20+ stones and even more liberties, but that'd
+  # be even more setup :)
+  benchmark.report 'connecting two small groups' do
+    stones = [stone1, stone2]
+    my_stone = 44
+    group_tracker.dup.join_group_of_friendly_stones(stones, my_stone)
+  end
+
+  benchmark.report 'add_liberties' do
+    liberties = [24, 78, 36, 79]
+    group_tracker.add_liberties(liberties, stone1)
+  end
+
+  enemy_stones = [56, 34]
+  liberties = {
+    stone1 => stone1,
+    23 => Rubykon::Board::EMPTY,
+    73 => Rubykon::Board::EMPTY
+  }
+  enemy_group_1 = Rubykon::Group.new(enemy_stones[0], [enemy_stones[0]], liberties, 2)
+  enemy_group_2 = Rubykon::Group.new(enemy_stones[1], [enemy_stones[1]], liberties.dup, 2)
+  group_tracker.stone_to_group[enemy_stones[0]] = 3
+  group_tracker.groups[3] = enemy_group_1
+
+  group_tracker.stone_to_group[enemy_stones[1]] = 4
+  group_tracker.groups[4] = enemy_group_2
+
+
+  # Does not trigger enemy_group.caught? and removing the group.
+  # That doesn't happen THAT often and it'd require to setup an according
+  # board (with each test run)
+  benchmark.report 'remove liberties of enemies' do
+
+    group_tracker.dup.take_liberties_of_enemies(enemy_stones, stone1, game.board, :black)
+  end
 end
