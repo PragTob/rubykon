a0285a74263c79f423a8ab6b1563d00e0a08a55a
Tobias Pfeiffer <pragtob@gmail.com>
2015-10-26 19:31:04 +0100

    Improve spec run time

--- spec/benchmark/avg/job_spec.rb
+++ spec/benchmark/avg/job_spec.rb
@@ -2,7 +2,7 @@ require_relative '../spec_helper'
 
 module Benchmark::Avg
   RSpec.describe Job do
-    let(:block) { proc {sleep 0.1}}
+    let(:block) { proc {sleep 0.01}}
     let(:label) {'some label'}
     subject(:job) {described_class.new label, block}
 
@@ -12,7 +12,7 @@ module Benchmark::Avg
       let(:fake_io) {FakeIO.new}
       before :each do
         $stdout = fake_io
-        job.run 0.2, 0.3
+        job.run 0.02, 0.03
         $stdout = STDOUT
       end
 
@@ -23,8 +23,8 @@ module Benchmark::Avg
       shared_examples_for 'static run time report' do
         it "gets the reportright" do
           expect(subject).to include label
-          expect(subject).to match /(60[0123]|59[6789])\.\d* i\/min/
-          expect(subject).to match /0\.1\d* s/
+          expect(subject).to match /(6[0123]\d\d|5[6789]\d\d)\.\d* i\/min/
+          expect(subject).to match /0\.01\d* s/
           expect(subject).to match /([012]\.\d*|0)%/
         end
       end
