888a4df807de518fc2dd669c46a9578e3a81127d
Tobias Pfeiffer <pragtob@gmail.com>
2015-11-08 15:25:20 +0100

    Run truffle benchmark last

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -1,13 +1,6 @@
 #! /bin/bash --login
 
 script_name=$1
-echo $script_name
-
-echo Running truffle graal with enough heap space
-jruby+truffle run --graal -- -e "puts RUBY_DESCRIPTION"
-jruby+truffle run --graal -J-Xmx1500m $script_name
-echo
-echo
 
 declare -A RUBY_TO_ARG=( ["2.2"]="" [jruby]="" [jruby-9]="--server -Xcompile.invokedynamic=true -J-Xmx1500m" [rbx]="" [jruby-1]="" [1.9.3]="" )
 
@@ -21,3 +14,8 @@ do
   echo
 done
 
+echo Running truffle graal with enough heap space
+jruby+truffle run --graal -- -e "puts RUBY_DESCRIPTION"
+jruby+truffle run --graal -J-Xmx1500m $script_name
+echo
+echo
