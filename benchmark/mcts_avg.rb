f60f4294aa0ac501ca09819210983333e368ab13
Tobias Pfeiffer <pragtob@gmail.com>
2015-11-08 15:24:22 +0100

    Use benchmark/avg for all major bord sizes

--- benchmark/mcts_avg.rb
+++ benchmark/mcts_avg.rb
@@ -2,10 +2,20 @@ require_relative '../lib/rubykon'
 require_relative '../lib/benchmark/avg'
 
 Benchmark.avg do |benchmark|
+  game_state_9 = Rubykon::GameState.new Rubykon::Game.new(9)
+  game_state_13 = Rubykon::GameState.new Rubykon::Game.new(13)
   game_state_19 = Rubykon::GameState.new Rubykon::Game.new(19)
   mcts = MCTS::MCTS.new
 
-  benchmark.config warmup: 40, time: 40
+  benchmark.config warmup: 110, time: 120
+
+  benchmark.report "9x9 10_000 iterations" do
+    mcts.start game_state_9, 10_000
+  end
+
+  benchmark.report "13x13 2_000 iterations" do
+    mcts.start game_state_13, 2_000
+  end
 
   benchmark.report "19x19 1_000 iterations" do
     mcts.start game_state_19, 1_000
