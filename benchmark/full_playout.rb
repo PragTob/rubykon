285abc0a6ef3207ae408ece66d2914145a03b1cc
Tobias Pfeiffer <pragtob@gmail.com>
2016-12-20 21:05:47 +0100

    Bugger other bord sizes, 19x19 is the true love

--- benchmark/full_playout.rb
+++ benchmark/full_playout.rb
@@ -5,12 +5,6 @@ require_relative 'support/benchmark-ips'
 Benchmark.ips do |benchmark|
   benchmark.config time: 30, warmup: 60
 
-  benchmark.report '9x9 full playout (+ score)' do
-    full_playout_for 9
-  end
-  benchmark.report '13x13 full playout (+ score)' do
-    full_playout_for 13
-  end
   benchmark.report '19x19 full playout (+ score)' do
     full_playout_for 19
   end
