71f2b969f87f7f2f843d6850163016a9f9617348
Tobias Pfeiffer <pragtob@gmail.com>
2017-01-16 21:18:45 +0100

    use new versions of ruby and working truffleruby

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -2,8 +2,8 @@
 
 script_name=$1
 
-declare -a RUBIES=( "2.0.0" "2.2.3" "jruby-9.0.3.0" "rbx-2.5.8" "2.3.3"
-"2.4.0" "jruby-9.1.6.0" "rbx-3.69" )
+declare -a RUBIES=( "2.0.0" "2.2.3" "jruby-9.0.3.0" "rbx-2.5.8"
+"2.4.0" "jruby-9.1.7.0" "rbx-3.69" )
 
 for ruby in "${RUBIES[@]}"
 do
@@ -28,14 +28,12 @@ done
 
 rvm use 2.3.3@rubykon
 echo Running old truffle graal with enough heap space
-GRAAL_BIN=~/dev/graalvm-jdk1.8.0/bin/java ../old_jruby/tool/jt.rb run --graal -e 'puts Truffle.graal?'
-GRAAL_BIN=~/dev/graalvm-jdk1.8.0/bin/java ../old_jruby/tool/jt.rb run --graal -e "puts RUBY_DESCRIPTION"
+GRAAL_BIN=~/dev/graalvm-jdk1.8.0/bin/java ../old_jruby/tool/jt.rb run --graal -e 'puts RUBY_DESCRIPTION; puts "Graal? #{Truffle::Graal.graal?}"'
 GRAAL_BIN=~/dev/graalvm-jdk1.8.0/bin/java ../old_jruby/tool/jt.rb run --graal -J-Xmx1500m $script_name
 echo
 echo
 
 rvm use 2.3.3
 echo Running new truffle head
-../jruby/bin/jruby-truffle-tool --graal-path ~/dev/graalvm-0.19-dk/bin/java run --graal -- -e 'puts Truffle::Graal.graal?'
-../jruby/bin/jruby-truffle-tool --graal-path ~/dev/graalvm-0.19-dk/bin/java run --graal -- -e  'puts RUBY_DESCRIPTION'
-../jruby/bin/jruby-truffle-tool --graal-path ~/dev/graalvm-0.19-dk/bin/java run --graal -- -J-Xmx1500m $script_name
+GRAAL_HOME=/home/tobi/github/truffleruby/graal/graal-core ./truffleruby/tool/jt.rb ruby --graal -e 'puts RUBY_DESCRIPTION; puts "Graal? #{Truffle::Graal.graal?}"'
+GRAAL_HOME=/home/tobi/github/truffleruby/graal/graal-core ../truffleruby/tool/jt.rb ruby --graal -J-Xmx1500m $script_name
