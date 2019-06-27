b0d7f2b5b4f8c0085d0d461c829a721364efed9a
Tobias Pfeiffer <pragtob@gmail.com>
2019-06-27 12:46:27 +0200

    Fixed new asdf setup script with current versions
    Sadly ruby 2.0 would require an openssl downgrade on my side
    so it's not too interesting imo

--- setup_all.sh
+++ setup_all.sh
@@ -1,18 +1,19 @@
 #!/bin/bash --login
-declare -a RUBIES=( "2.0.0" "2.2.3" "jruby-9.0.3.0" "rbx-2.5.8"
-"2.4.0" "jruby-9.1.7.0" "rbx-3.69" )
+declare -a RUBIES=( "2.4.6" "2.6.3" "2.7.0-dev" "jruby-9.1.17.0" "jruby-9.2.7.0" "truffleruby-19.0.0" "truffleruby-1.0.0-rc16")
 
 for ruby in "${RUBIES[@]}"
 do
   echo Running $ruby
-  rvm use $ruby
+  asdf install ruby $ruby
+  asdf local ruby $ruby
   ruby -v
-  # new rbx versions crash with new bundler so gotta use the bundled 1.11.x
-  # https://github.com/rubinius/rubinius/issues/3710
-  if [ "$ruby" != "rbx-3.69" ]
-  then
-    gem install bundler
-  fi
+  # # new rbx versions crash with new bundler so gotta use the bundled 1.11.x
+  # # https://github.com/rubinius/rubinius/issues/3710
+  # if [ "$ruby" != "rbx-3.69" ]
+  # then
+  #   gem install bundler
+  # fi
+  gem install bundler
   bundle install
   bundle exec rspec
   echo
