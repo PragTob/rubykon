1dff70f8f5824cae9bd14e8e6cd35c3c24a9192a
Tobias Pfeiffer <pragtob@gmail.com>
2019-07-07 16:12:54 +0200

    give me some warmup

--- benchmark/full_playout.rb
+++ benchmark/full_playout.rb
@@ -3,7 +3,7 @@ require_relative 'support/playout_help'
 require_relative 'support/benchmark-ips'
 
 Benchmark.ips do |benchmark|
-  benchmark.config time: 30, warmup: 60
+  benchmark.config time: 180, warmup: 120
 
   benchmark.report '19x19 full playout (+ score)' do
     full_playout_for 19
