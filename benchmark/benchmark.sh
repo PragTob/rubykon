5380bbed8d2bb62c58dcc36d7dde1a3dff06a167
Tobias Pfeiffer <pragtob@gmail.com>
2019-07-14 10:53:23 +0200

    JRubis x JVMs

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -1,54 +1,71 @@
 #! /bin/bash --login
 
+set -e
+
 script_name=$1
 
-declare -a RUBIES=( "2.4.6" "2.6.3" "2.7.0-dev"  "jruby-9.1.17.0" "jruby-9.2.7.0" "truffleruby-19.0.0")
+# declare -a RUBIES=( "2.4.6" "2.6.3" "2.7.0-dev"  "truffleruby-19.0.0")
 
-for ruby in "${RUBIES[@]}"
+# for ruby in "${RUBIES[@]}"
+# do
+#   echo Running $ruby
+#   asdf local ruby $ruby
+#   ruby -v
+#   ruby $script_name
+#   echo
+#   echo "--------------------------------------------------"
+#   echo
+# done
+
+# echo "-------------------------------------------"
+# echo "|             DONE WITH NORMAL RUBIES     |"
+# echo "-------------------------------------------"
+
+declare -a JRUBIES=("jruby-9.1.17.0" "jruby-9.2.7.0")
+declare -a JAVAS=("adoptopenjdk-8.212" "openjdk-12.0.1" "oracle-8.141"  "corretto-8.212" "corretto-11.0.3")
+
+for java in "${JAVAS[@]}"
 do
-  echo Running $ruby
-  asdf local ruby $ruby
-  ruby -v
-  ruby $script_name
+  echo "Using Java $java"
+  asdf local java $java
+  java -version
+
+  for ruby in "${JRUBIES[@]}"
+  do
+    asdf local ruby $ruby
+    echo Running $ruby
+    ruby -v
+    ruby $script_name
+    echo
+
+    echo Running $ruby with --server -Xcompile.invokedynamic=true -J-Xmx1500m
+    ruby --server -Xcompile.invokedynamic=true -J-Xmx1500m $script_name
+    echo
+    echo "--------------------------------------------------"
+    echo
+  done
+  echo "-"
   echo
-  echo "--------------------------------------------------"
   echo
 done
 
-echo "-------------------------------------------"
-echo "|             DONE WITH NORMAL RUBIES     |"
-echo "-------------------------------------------"
+# echo "-------------------------------------------"
+# echo "|             DONE WITH JRUBIES          |"
+# echo "-------------------------------------------"
 
+# declare -a JITRUBIES=( "2.6.3" "2.7.0-dev" )
 
-declare -a JRUBIES=("jruby-9.1.17.0" "jruby-9.2.7.0" )
-for ruby in "${JRUBIES[@]}"
-do
-  echo Running $ruby with --server -Xcompile.invokedynamic=true -J-Xmx1500m
-  asdf local ruby $ruby
-  ruby -v
-  ruby --server -Xcompile.invokedynamic=true -J-Xmx1500m $script_name
-  echo
-  echo "--------------------------------------------------"
-  echo
-done
+# for ruby in "${JITRUBIES[@]}"
+# do
+#   echo Running $ruby
+#   asdf local ruby $ruby
+#   ruby -v
+#   ruby --jit $script_name
+#   echo
+#   echo "--------------------------------------------------"
+#   echo
+# done
 
-echo "-------------------------------------------"
-echo "|             DONE WITH ID RUBIES          |"
-echo "-------------------------------------------"
-
-declare -a JITRUBIES=( "2.6.3" "2.7.0-dev" )
-
-for ruby in "${JITRUBIES[@]}"
-do
-  echo Running $ruby
-  asdf local ruby $ruby
-  ruby -v
-  ruby --jit $script_name
-  echo
-  echo "--------------------------------------------------"
-  echo
-done
-
-echo "-------------------------------------------"
-echo "|             DONE WITH JIT RUBIES          |"
-echo "-------------------------------------------"
+# echo "-------------------------------------------"
+# echo "|             DONE WITH JIT RUBIES          |"
+# echo "-------------------------------------------"
