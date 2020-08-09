2cbe2089cf9472cdbd48fc5a2f76cb7846250d42
Tobias Pfeiffer <pragtob@gmail.com>
2020-08-09 21:39:15 +0200

    reorganize and redefine slimmed down JVM selection

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -21,6 +21,21 @@ echo "-------------------------------------------"
 echo "|            DONE WITH 'NORMAL' RUBIES     |"
 echo "-------------------------------------------"
 
+asdf local ruby trufflerubyVM
+echo "Running GraalVM installed ruby --native"
+ruby --native -v
+ruby --native $script_name
+echo
+
+echo "Running GraalVM installed ruby --jvm"
+ruby --jvm -v
+ruby --jvm $script_name
+echo
+
+echo "-------------------------------------------"
+echo "|            DONE WITH TuffleRuby VM       |"
+echo "-------------------------------------------"
+
 declare -a MJITRUBIES=( "2.6.6" "2.7.1" "2.8.0-dev" )
 
 for ruby in "${MJITRUBIES[@]}"
@@ -39,7 +54,9 @@ echo "|             DONE WITH MJIT RUBIES         |"
 echo "-------------------------------------------"
 
 declare -a JRUBIES=("jruby-9.1.17.0" "jruby-9.2.11.1")
-declare -a JAVA8S=( "adoptopenjdk-8.0.265+1" "adoptopenjdk-8.0.265+1.openj9-0.21.0"  "java-se-ri-8u41-b04" "corretto-8.265.01.1" "dragonwell-8.4.4" "graalvm-20.1.0+java8")
+# declare -a JAVA8S=( "adoptopenjdk-8.0.265+1" "adoptopenjdk-8.0.265+1.openj9-0.21.0"  "java-se-ri-8u41-b04" "corretto-8.265.01.1" "dragonwell-8.4.4" "graalvm-20.1.0+java8")
+declare -a JAVA8S=( "adoptopenjdk-8.0.265+1" "java-se-ri-8u41-b04" "graalvm-20.1.0+java8")
+
 
 for java in "${JAVA8S[@]}"
 do
@@ -70,7 +87,9 @@ do
   echo
 done
 
-declare -a JAVA9PLUSS=( "adoptopenjdk-14.0.2+12" "adoptopenjdk-14.0.2+12.openj9-0.21.0"  "java-se-ri-14+36" "corretto-11.0.8.10.1" "dragonwell-11.0.7.2+9" "graalvm-20.1.0+java11")
+# declare -a JAVA9PLUSS=( "adoptopenjdk-14.0.2+12" "adoptopenjdk-14.0.2+12.openj9-0.21.0"  "java-se-ri-14+36" "corretto-11.0.8.10.1" "dragonwell-11.0.7.2+9" "graalvm-20.1.0+java11")
+declare -a JAVA9PLUSS=( "adoptopenjdk-14.0.2+12" "java-se-ri-14+36" "graalvm-20.1.0+java11")
+
 
 for java in "${JAVA9PLUSS[@]}"
 do
@@ -109,14 +128,3 @@ done
 echo "-------------------------------------------"
 echo "|             DONE WITH JRUBIES          |"
 echo "-------------------------------------------"
-
-asdf local ruby trufflerubyVM
-echo "Running GraalVM installed ruby --native"
-ruby --native -v
-ruby --native $script_name
-echo
-
-echo "Running GraalVM installed ruby --jvm"
-ruby --jvm -v
-ruby --jvm $script_name
-echo
